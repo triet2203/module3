5a2b087faba83a5a69e1e119322c6c2906beead4
triet2203 <minhtriet2203@gmail.com>
2026-06-23 11:42:32 +0700

    nop bai tap 1 ss2

---
+++ database/ss1_bai_1.sql
@@ -0,0 +1,15 @@
+create database bai_tap;
+
+use bai_tap;
+
+create table class(
+	id int,
+    name varchar(200)
+);
+
+create table teacher(
+	id int,
+    name varchar(200),
+    age int,
+    country varchar(50)
+);
